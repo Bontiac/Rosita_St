ef42dae09353b01f6c853137c197ec080ae2670e
Bontiac <alejandra.davilar@konradlorenz.edu.co>
2021-05-21 21:41:50 +0200

    changes_auth

--- src/main/resources/templates/log_in/log_in.js
+++ src/main/resources/templates/log_in/log_in.js
@@ -1,16 +1,38 @@
-firebase.auth().onAuthStateChanged(function(user) {
+<script src="https://www.gstatic.com/firebasejs/5.9.1/firebase-app.js"></script>
+
+firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      // User is signed in.
+        window.alert('user logged in')
     } else {
-      // No user is signed in.
+        // No user is signed in.
     }
-  });
+});
 
-function login(){
+function login() {
 
-var userEmail = document.getElementById('email_field').value;
-var userPass = document.getElementById('password_field').value;
+    var userEmail = document.getElementById('email_field').value;
+    var userPass = document.getElementById('password_field').value;
+    const auth = firebase.auth();
+    auth.signInWithEmailAndPassword(email, password)
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
 
-window.alert(userEmail);
 
+}
+function register() {
+
+    var userEmail = document.getElementById('email_field').value;
+    var userPass = document.getElementById('password_field').value;
+    const auth = firebase.auth();
+    auth.createUserWithEmailAndPassword(userEmail, 
+        userPass).then(function(user) {
+           var user = firebase.auth().currentUser;
+           console.log(user); // Optional
+        }, function(error) {
+           // Handle Errors here.
+           var errorCode = error.code;
+           var errorMessage = error.message;
+        });
 }
