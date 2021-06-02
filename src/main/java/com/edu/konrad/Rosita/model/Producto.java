5d84f7d9f863608200311545f78900b265fcaa36
Daniel Andres Fierro Velasquez <daniela.fierrov@konradlorenz.edu.co>
2021-06-02 15:57:55 -0500

    Creando el modelo de producto

---
+++ src/main/java/com/edu/konrad/Rosita/model/Producto.java
@@ -0,0 +1,23 @@
+package com.edu.konrad.Rosita.model;
+
+public class Producto {
+    private String nombre;
+    private int valor;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAValor() {
+        return valor;
+    }
+
+    public void setApellido(int valor) {
+        this.valor = valor;
+    }
+
+}
