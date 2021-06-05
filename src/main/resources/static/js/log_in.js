cf2297a02f2cfea37aa2b19dc7f0bc5e7f327a0a
Bontiac <alejandra.davilar@konradlorenz.edu.co>
2021-06-05 20:14:22 +0200

    added user admin views for products

--- src/main/resources/static/js/log_in.js
+++ src/main/resources/static/js/log_in.js
@@ -1,59 +1,67 @@
 firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        window.alert('user logged in')
-        document.getElementById("user_loggedOut").style.display = "block";
-        document.getElementById("user_loggedIn").style.display = "none";
-    
-        var user = firebase.auth().currentUser;
-    } else {
-        // No user is signed in.
-        document.getElementById("user_loggedOut").style.display = "none";
-        document.getElementById("user_loggedIn").style.display = "block";
-    
+  if (user) {
+    window.alert('user logged in')
+    document.getElementById("user_loggedOut").style.display = "block";
+    document.getElementById("user_loggedIn").style.display = "none";
+    document.getElementById("admin_view").style.display = "none";
+    if (currentUser.toString() == "rositapulido25@gmail.com") {
+      document.getElementById("admin_view").style.display = "block";
     }
+    var user = firebase.auth().currentUser;
+  } else {
+    // No user is signed in.
+    document.getElementById("user_loggedOut").style.display = "none";
+    document.getElementById("user_loggedIn").style.display = "block";
+    document.getElementById("admin_view").style.display = "none";
+  }
 });
 
-function login(){
+function login() {
 
-    var userEmail = document.getElementById("email_field").value;
-    var userPass = document.getElementById("password_field").value;
-  
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)  .then((userCredential) => {
-      // Signed in 
-      var user = userCredential.user;
-      location.href="/all";
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
 
-      // ...
-    }).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    if (currentUser.toString() == "rositapulido25@gmail.com") {
+      location.href = "/all";
+    } else {
+      location.href = "/";
+
+    }
+
+
+    // ...
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+
+function register() {
+
+  var userEmail = document.getElementById('email_field').value;
+  var userPass = document.getElementById('password_field').value;
+  const auth = firebase.auth();
+  auth.createUserWithEmailAndPassword(userEmail,
+    userPass).then(function (user) {
+      var user = firebase.auth().currentUser;
+      console.log(user); // Optional
+    }, function (error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-  
-      // ...
     });
-   
-  }
-
-  
-function register() {
-
-    var userEmail = document.getElementById('email_field').value;
-    var userPass = document.getElementById('password_field').value;
-    const auth = firebase.auth();
-    auth.createUserWithEmailAndPassword(userEmail, 
-        userPass).then(function(user) {
-           var user = firebase.auth().currentUser;
-           console.log(user); // Optional
-        }, function(error) {
-           // Handle Errors here.
-           var errorCode = error.code;
-           var errorMessage = error.message;
-        });
 }
-function logout(){
-    firebase.auth().signOut();
-    window.alert('user logout')
-  }
-  
+function logout() {
+  firebase.auth().signOut();
+  window.alert('user logout')
+}
